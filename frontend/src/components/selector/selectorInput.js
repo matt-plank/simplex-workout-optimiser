af254cafc1b0bf96332cd1fcf596cb9e11e1c3e4
matt-plank <matthew_plank@outlook.com>
2023-02-27 20:56:07 +0000

    Updated exercise selection row

--- frontend/src/components/selector/selectorInput.js
+++ frontend/src/components/selector/selectorInput.js
@@ -1,13 +1,37 @@
-import { AiOutlinePlus } from "react-icons/ai";
-
-const SelectorInput = () => {
+const SelectorInput = ({ sets }) => {
   return (
     <div className="selector-input">
-      <button className="new-row">
-        <AiOutlinePlus />
-      </button>
-      <input type="text" placeholder="Exercise Name" />
-      <input type="text" placeholder="Muscle Groups" />
+      <select>
+        <option value="">Select</option>
+        <optgroup label="Push">
+          <option value="">Bench Press</option>
+          <option value="">Dumbbell Incline Press</option>
+          <option value="">Shoulder Press</option>
+          <option value="">Dips</option>
+          <option value="">Cable Flys</option>
+          <option value="">Tricep Pushdown</option>
+        </optgroup>
+        <optgroup label="Pull">
+          <option value="">Pull-ups</option>
+          <option value="">Chin-ups</option>
+          <option value="">Lat Pulldown</option>
+          <option value="">Cable Rows</option>
+          <option value="">Lateral Raises</option>
+          <option value="">Seated Lateral Raises</option>
+          <option value="">Face Pulls</option>
+        </optgroup>
+        <optgroup label="Legs">
+          <option value="">Squats</option>
+          <option value="">Bulgarian Split Squats</option>
+          <option value="">Lunges</option>
+          <option value="">RDLs</option>
+          <option value="">Leg Extensions</option>
+          <option value="">Hamstring Curls</option>
+          <option value="">Calf Raises</option>
+        </optgroup>
+      </select>
+
+      <input type="text" placeholder="Sets" value={sets} />
     </div>
   );
 };
