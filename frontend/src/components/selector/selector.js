4dc587d8af31b4a2f335e9978733281b1e252cd4
matt-plank <matthew_plank@outlook.com>
2023-02-27 21:35:23 +0000

    Updated form styling

--- frontend/src/components/selector/selector.js
+++ frontend/src/components/selector/selector.js
@@ -7,6 +7,7 @@ const Selector = () => {
       <div className="selector">
         <SelectorInput />
 
+        <button className="new-row">New Exercise</button>
         <button className="submit">Optimise</button>
       </div>
     </div>
