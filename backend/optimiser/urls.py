b1c3e92f9837a1d924632673319fddd89afe0619
matt-plank <matthew_plank@outlook.com>
2023-02-28 01:58:12 +0000

    Updated dummy endpoint to use proper request schema

--- backend/optimiser/urls.py
+++ backend/optimiser/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_optimised_workout),
+    path("", views.create_optimised_workout),
 ]
--- backend/optimiser/views.py
+++ backend/optimiser/views.py
@@ -1,7 +1,19 @@
+import json
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(["GET"])
-def get_optimised_workout(request):
-    return Response({"message": "Hello, World!"})
+@api_view(["POST"])
+def create_optimised_workout(request):
+    """Create a new "optimised workout"."""
+    request_json: list = json.loads(request.body)
+
+    # request_json = [{"name": "..."}]
+
+    for i, exercise in enumerate(request_json):
+        exercise["sets"] = 10
+
+        request_json[i] = exercise
+
+    return Response(request_json)
